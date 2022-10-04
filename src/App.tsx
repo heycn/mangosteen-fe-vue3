c48b71ed84db996ad80317c14f3828ebcb737bb7
heycn <heycn@foxmail.com>
2022-10-04 16:26:48 +0800

    使用 tsx

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { defineComponent, ref } from 'vue'
+
+export const App = defineComponent({
+  setup() {
+    const refCount = ref(0)
+    const onClick = () => refCount.value++
+
+    return () => <>
+			<div>{refCount.value}</div>
+			<div>
+				<button onClick={onClick}>+1</button>
+			</div>
+		</>
+  }
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    vueJsx({
+      transformOn: true,
+      mergeProps: true
+    })
+  ]
 })
