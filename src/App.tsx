e5310f6aca7d8a51b6ab8b5d2dccfbc51ac5a20d
heycn <heycn@foxmail.com>
2022-10-04 22:08:53 +0800

    解决 Edge 浏览器一开始不能全屏的 bug

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,9 @@ import './App.scss'
 export const App = defineComponent({
   setup() {
     return () => (
-      <div>
+      <main>
         <RouterView />
-      </div>
+      </main>
     )
   }
 })
