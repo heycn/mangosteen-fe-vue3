7d94ada14f87351d7950cb76d4d8e30c1b4280cd
heycn <heycn@foxmail.com>
2022-10-04 17:10:24 +0800

    使用 vue-router 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import { defineComponent, ref } from 'vue'
+import { RouterView } from 'vue-router'
 
 export const App = defineComponent({
   setup() {
-    const refCount = ref(0)
-    const onClick = () => refCount.value++
-
-    return () => <>
-			<div>{refCount.value}</div>
-			<div>
-				<button onClick={onClick}>+1</button>
-			</div>
-		</>
+    return () => (
+      <>
+        <header>
+          <ul>
+            <li>
+              <router-link to='/'>Foo</router-link>
+            </li>
+            <li>
+              <router-link to='bar'>Bar</router-link>
+            </li>
+          </ul>
+        </header>
+        <div>
+          <RouterView />
+        </div>
+        <footer>页脚</footer>
+      </>
+    )
   }
 })
---
+++ src/views/Bar.tsx
@@ -0,0 +1,9 @@
+import { defineComponent } from 'vue'
+
+export const Bar = defineComponent({
+	setup() {
+		return () => <>
+			<div>Bar</div>
+		</>
+	}
+})
