f8c04c6ca9461e71afdb28a3d1232a3eaed91d63
heycn <heycn@foxmail.com>
2022-10-04 15:29:08 +0800

    一键部署 github

--- deploy_github.sh
+++ deploy_github.sh
@@ -1,11 +1,11 @@
-rm -rf dist
-pnpm run build
-cd /dist
-git init
-git add -A
-git commit -m 'deploy'
-git branch -M master
-git remote add origin git@github.com:heycn/keeping-book-website.git
-git push -f -u origin master
-cd -
+rm -rf dist &&
+pnpm run build &&
+cd dist &&
+git init &&
+git add . &&
+git commit -m 'deploy' &&
+git branch -M master &&
+git remote add origin git@github.com:heycn/keeping-book-website.git &&
+git push -f origin master &&
+cd - &&
 echo https://heycn.github.io/keeping-book-website/
