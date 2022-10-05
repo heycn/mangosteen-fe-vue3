040666fb84e5b503ab1cde83b09f2493a7f5f0de
heycn <heycn@foxmail.com>
2022-10-05 23:27:20 +0800

    改变路由切换形式

--- src/components/welcome/FirstActions.tsx
+++ src/components/welcome/FirstActions.tsx
@@ -1,12 +1,18 @@
-import s from './welcome.module.scss';
-import { RouterLink } from 'vue-router';
-import { FunctionalComponent } from 'vue';
+import s from './welcome.module.scss'
+import { RouterLink, useRouter } from 'vue-router'
+import { FunctionalComponent } from 'vue'
+
 export const FirstActions: FunctionalComponent = () => {
-  return <div class={s.actions}>
-    <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
-    <RouterLink to="/welcome/2" >下一页</RouterLink>
-    <RouterLink to="/start" >跳过</RouterLink>
-  </div>
+  const router = useRouter()
+  const handleNext = () => router.replace('/welcome/2')
+
+  return (
+    <div class={s.actions}>
+      <RouterLink class={s.fake} to='/start'>跳过</RouterLink>
+      <button onClick={handleNext}>下一页</button>
+      <RouterLink to='/start'>跳过</RouterLink>
+    </div>
+  )
 }
 
 FirstActions.displayName = 'FirstActions'
--- src/components/welcome/ForthActions.tsx
+++ src/components/welcome/ForthActions.tsx
@@ -1,11 +1,20 @@
-import s from './welcome.module.scss';
-import { RouterLink } from 'vue-router';
-export const ForthActions = () => (
-  <div class={s.actions}>
-    <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
-    <RouterLink to="/start" >完成</RouterLink>
-    <RouterLink class={s.fake} to="/start" >跳过</RouterLink>
-  </div>
-)
+import s from './welcome.module.scss'
+import { RouterLink, useRouter } from 'vue-router'
+import { FunctionalComponent } from 'vue'
+
+export const ForthActions: FunctionalComponent = () => {
+  const router = useRouter()
+  const handleNext = () => router.replace('/start')
+
+  return (
+    <div class={s.actions}>
+      <RouterLink class={s.fake} to='/start'>跳过</RouterLink>
+      <button onClick={handleNext}>完成</button>
+      <RouterLink class={s.fake} to='/start'>
+        跳过
+      </RouterLink>
+    </div>
+  )
+}
 
 ForthActions.displayName = 'ForthActions'
