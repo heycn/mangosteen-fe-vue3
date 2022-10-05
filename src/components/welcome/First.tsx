069f9e13dd6876133797792767f73fc7f01eb943
heycn <heycn@foxmail.com>
2022-10-05 17:56:33 +0800

    缩进

--- src/components/welcome/First.tsx
+++ src/components/welcome/First.tsx
@@ -5,7 +5,7 @@ import { FunctionalComponent } from 'vue'
 export const First: FunctionalComponent = () => (
   <div class={s.card}>
     <img src={icon} />
-    <h2>别看我只是一头猪 <br />我可是一头会省钱的猪</h2>
+    <h2>别看我只是一头猪<br />我可是一头会省钱的猪</h2>
   </div>
 )
 
