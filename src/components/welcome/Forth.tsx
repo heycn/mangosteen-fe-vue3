d40de4a40528b284b0429140e2ba0507b14b714e
heycn <heycn@foxmail.com>
2022-10-05 13:43:06 +0800

    是个欢迎页面搞定

--- src/components/welcome/Forth.tsx
+++ src/components/welcome/Forth.tsx
@@ -1,10 +1,26 @@
 import { defineComponent } from 'vue'
+import s from './First.module.scss'
+import icon from '../../assets/icons/welcome_4.svg'
+import { RouterLink } from 'vue-router'
+
 export const Forth = defineComponent({
-	setup: () => {
-		return () => (
-			<div>
-				Forth
-			</div>
-		)
-	}
+  setup: () => {
+    return () => (
+      <div class={s.wrapper}>
+        <div class={s.card}>
+          <img src={icon} />
+          <h2>
+            别看我只是一头猪
+            <br />
+            我可是一头会省钱的猪
+          </h2>
+        </div>
+        <div class={s.actions}>
+          <span class={s.fake}>占位</span>
+          <RouterLink to='/start'>完成</RouterLink>
+          <span class={s.fake}>占位</span>
+        </div>
+      </div>
+    )
+  }
 })
--- src/components/welcome/Second.tsx
+++ src/components/welcome/Second.tsx
@@ -1,10 +1,22 @@
 import { defineComponent } from 'vue'
+import s from './First.module.scss'
+import icon from '../../assets/icons/welcome_2.svg'
+import { RouterLink } from 'vue-router'
+
 export const Second = defineComponent({
-	setup: () => {
-		return () => (
-			<div>
-				Second
-			</div>
-		)
-	}
+  setup: () => {
+    return () => (
+      <div class={s.wrapper}>
+        <div class={s.card}>
+          <img src={icon} />
+          <h2>每日提醒<br />养成定期记账习惯</h2>
+        </div>
+        <div class={s.actions}>
+          <span class={s.fake}>占位</span>
+          <RouterLink to='/welcome/3'>下一页</RouterLink>
+          <RouterLink to='/start'>跳过</RouterLink>
+        </div>
+      </div>
+    )
+  }
 })
