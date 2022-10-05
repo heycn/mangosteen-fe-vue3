20362f62e5e1dc1ab5b5f50dd6f87f2b7b399cca
heycn <heycn@foxmail.com>
2022-10-06 00:04:38 +0800

    改为 button

--- src/components/welcome/ForthActions.tsx
+++ src/components/welcome/ForthActions.tsx
@@ -1,18 +1,16 @@
 import s from './welcome.module.scss'
-import { RouterLink, useRouter } from 'vue-router'
+import { useRouter } from 'vue-router'
 import { FunctionalComponent } from 'vue'
 
 export const ForthActions: FunctionalComponent = () => {
   const router = useRouter()
-  const handleNext = () => router.replace('/start')
+  const replace = (to: string) => router.replace(to)
 
   return (
     <div class={s.actions}>
-      <RouterLink class={s.fake} to='/start'>跳过</RouterLink>
-      <button onClick={handleNext}>完成</button>
-      <RouterLink class={s.fake} to='/start'>
-        跳过
-      </RouterLink>
+      <button class={s.fake}>跳过</button>
+      <button onClick={() => replace('start')}>开始</button>
+      <button class={s.fake}>跳过</button>
     </div>
   )
 }
