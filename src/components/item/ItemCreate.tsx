0aba55843c608a3dc19ecea8063d43367908ba8c
heycn <heycn@foxmail.com>
2022-10-06 19:52:40 +0800

    wip: items 页面

--- src/components/item/ItemCreate.tsx
+++ src/components/item/ItemCreate.tsx
@@ -1,18 +1,33 @@
-import { defineComponent, PropType } from 'vue'
+import { defineComponent, PropType, ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { MainLayout } from '../../layouts/MainLayout'
+import { Icon } from '../../shared/Icon'
 import s from './ItemCreate.module.scss'
 
 export const ItemCreate = defineComponent({
-	props: {
-		name: {
-			type: String as PropType<string>
-		}
-	},
+  props: {
+    name: {
+      type: String as PropType<string>
+    }
+  },
 
-	setup: (props, context) => {
-		return () => (
-			<div>
-				ItemPage				
-			</div>
-		)
-	}
+  setup: (props, context) => {
+    const overlayVisible = ref(false)
+    const router = useRouter()
+    const onClickMenu = () => {
+      overlayVisible.value = !overlayVisible.value
+    }
+
+    return () => (
+      <MainLayout>
+        {{
+          title: () => 'Give My Money',
+          icon: () => <Icon name='menu' class={s.icon} onClick={onClickMenu} />,
+          default: () => (
+						<div>main</div>
+          )
+        }}
+      </MainLayout>
+    )
+  }
 })
--- src/views/ItemPage.tsx
+++ src/views/ItemPage.tsx
@@ -1,18 +1,9 @@
-import { defineComponent, PropType } from 'vue'
+import { defineComponent } from 'vue'
+import { RouterView } from 'vue-router'
 import s from './ItemPage.module.scss'
 
 export const ItemPage = defineComponent({
-	props: {
-		name: {
-			type: String as PropType<string>
-		}
-	},
-
-	setup: (props, context) => {
-		return () => (
-			<div>
-				ItemPage				
-			</div>
-		)
+	setup: () => {
+		return () => (<RouterView />)
 	}
 })
