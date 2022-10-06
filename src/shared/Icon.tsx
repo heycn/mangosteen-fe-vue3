79166ca9d5e42fa15201a428cf3d01e0c1f16955
heycn <heycn@foxmail.com>
2022-10-06 18:15:20 +0800

    创建 Overlay 组件

--- src/shared/Icon.tsx
+++ src/shared/Icon.tsx
@@ -6,13 +6,16 @@ export const Icon = defineComponent({
     name: {
       type: String as PropType<string>,
       required: true
+    },
+    onClick: {
+      type: Function as PropType<(e: Event) => void>
     }
   },
 
   setup: (props, context) => {
     return () => (
-      <svg class={s.icon}>
-        <use xlinkHref={`#${props.name}`}></use>
+      <svg class={s.svg}>
+        <use xlinkHref={`#${props.name}`} onClick={props.onClick}></use>
       </svg>
     )
   }
--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -1,23 +1,30 @@
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 import { Button } from '../shared/Button'
 import s from './StartPage.module.scss'
 import { FloatButton } from '../shared/FloatButton'
 import { Center } from '../shared/Center'
 import { Icon } from '../shared/Icon'
 import { Navbar } from '../shared/Navbar'
+import { Overlay } from '../shared/Overlay'
 
 export const StartPage = defineComponent({
   setup: (props, context) => {
+    const overlayVisible = ref(false)
+
     const onClick = () => {
       console.log('click')
+    }
+
+    const onClickMenu = () => {
+      overlayVisible.value = !overlayVisible.value
     }
 
     return () => (
       <div>
         <Navbar>
           {{
-            default: 'Give My Money',
-            icon: <Icon name='menu' class={s.icon} />
+            default: () => 'Give My Money',
+            icon: () => <Icon name='menu' class={s.icon} onClick={onClickMenu} />
           }}
         </Navbar>
         <Center class={s.logo_wrapper}>
@@ -29,6 +36,7 @@ export const StartPage = defineComponent({
           </Button>
         </div>
         <FloatButton iconName='add' />
+        {overlayVisible.value && <Overlay onClose={() => (overlayVisible.value = false)} />}
       </div>
     )
   }
