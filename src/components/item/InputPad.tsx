d24e826d678f8646a64f0f8033ebb5fe76d64591
heycn <heycn@foxmail.com>
2022-10-06 22:19:18 +0800

    给 date 和 amount 加样式

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -31,12 +31,12 @@ export const InputPad = defineComponent({
 
     return () => (
       <div class={s.wrapper}>
-        <div class={s.notes}>
-          <span>
-            <Icon name='date' />
+        <div class={s.dateAntAmount}>
+          <span class={s.date}>
+            <Icon name='date' class={s.icon}/>
             <span>2022-01-01</span>
           </span>
-          <span class={s.amount}>数字</span>
+          <span class={s.amount}>123.2</span>
         </div>
         <div class={s.buttons}>
           {buttons.map(button => (
