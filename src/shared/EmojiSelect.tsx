ece1fc415daf5059cd673b71e61d923df23acecc
heycn <heycn@foxmail.com>
2022-10-07 20:23:44 +0800

    完成 EmojiSelect 组件

--- src/shared/EmojiSelect.tsx
+++ src/shared/EmojiSelect.tsx
@@ -1,35 +1,50 @@
-import { defineComponent, ref } from 'vue'
+import { computed, defineComponent, ref } from 'vue'
 import { emojiList } from './emojiList'
 import s from './EmojiSelect.module.scss'
 export const EmojiSelect = defineComponent({
+  props: {
+    modelValue: {
+      type: String
+    }
+  },
 
-  setup: () => {
-    console.log(emojiList)
-    const refSelected = ref(1)
+  setup: (props, context) => {
+    const refSelected = ref(0)
     const table: [string, string[]][] = [
       ['表情', ['face-smiling', 'face-affection', 'face-tongue', 'face-hand', 'face-neutral-skeptical', 'face-sleepy', 'face-unwell', 'face-hat', 'face-glasses', 'face-concerned', 'face-negative', 'face-costume']],
       ['手势', ['hand-fingers-open', 'hand-fingers-partial', 'hand-single-finger', 'hand-fingers-closed', 'hands', 'hand-prop', 'body-parts']],
       ['人物', ['person', 'person-gesture', 'person-role', 'person-fantasy', 'person-activity', 'person-sport', 'person-resting']],
       ['衣服', ['clothing']],
       ['动物', ['cat-face', 'monkey-face', 'animal-mammal', 'animal-bird', 'animal-amphibian', 'animal-reptile', 'animal-marine', 'animal-bug']],
-		  ['植物', ['plant-flower', 'plant-other']],
+      ['植物', ['plant-flower', 'plant-other']],
       ['自然', ['sky & weather', 'science']],
       ['食物', ['food-fruit', 'food-vegetable', 'food-prepared', 'food-asian', 'food-marine', 'food-sweet']],
-			['运动', ['sport', 'game']]
+      ['运动', ['sport', 'game']]
     ]
-    const selectedItem = table[refSelected.value][1]
-    const emojis = selectedItem.map(category =>
-      emojiList.find(item => item[0] === category)?.[1].map(item => <li>{item}</li>)
-    )
+    const emojis = computed(() => {
+      const selectedItem = table[refSelected.value][1]
+      return selectedItem.map(category =>
+        emojiList.find(item => item[0] === category)
+          ?.[1]
+          .map(item => <li  class={item === props.modelValue ? s.selectedEmoji : ''} onClick={() => selectEmoji(item)}>{item}</li>)
+      )
+    })
+    const selectCategory = (index: number) => refSelected.value = index
+    const selectEmoji = (emoji: string) => context.emit('update:modelValue', emoji)
 
     return () => (
       <div class={s.emojiList}>
         <nav>
-          {table.map(item => (
-            <span>{item[0]}</span>
+          {table.map((item, index) => (
+            <span
+              class={index === refSelected.value ? s.selected : ''}
+              onClick={() => selectCategory(index)}
+            >
+              {item[0]}
+            </span>
           ))}
         </nav>
-        <ol>{emojis}</ol>
+        <ol>{emojis.value}</ol>
       </div>
     )
   }
