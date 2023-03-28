d6fc975891fd23466d14ce43eb8a15d51d61c1c6
heycn <heycn@foxmail.com>
2023-03-28 10:06:15 +0800

    diff

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import styleImport, { VantResolve } from 'vite-plugin-style-import'
+import styleImport, { VantResolve } from 'vite-plugin-style-import';
 // @ts-nocheck
-import { svgstore } from './src/vite_plugins/svgstore'
+import { svgstore } from './src/vite_plugins/svgstore';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,8 +15,8 @@ export default defineConfig({
     }),
     svgstore(),
     styleImport({
-      resolves: [VantResolve()]
-    })
+      resolves: [VantResolve()],
+    }),
   ],
   server: {
     proxy: {
