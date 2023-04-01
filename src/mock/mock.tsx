ba8fcf595b472c6d065ca80f39ff615f25f8ac80
heycn <heycn@foxmail.com>
2023-04-01 23:53:10 +0800

    可以编辑标签

--- src/mock/mock.tsx
+++ src/mock/mock.tsx
@@ -4,6 +4,17 @@ import { AxiosRequestConfig } from 'axios';
 type Mock = (config: AxiosRequestConfig) => [number, any]
 
 faker.setLocale('zh_CN');
+export const mockTagShow: Mock = config =>{
+  const createTag = (attrs?: any) =>
+    ({
+      id: createId(),
+      name: faker.lorem.word(),
+      sign: faker.internet.emoji(),
+      kind: 'expenses',
+      ...attrs
+    })
+  return [200, {resource: createTag()}]
+}
 
 export const mockItemCreate: Mock = config => {
   return [200, {
@@ -20,7 +31,6 @@ export const mockItemCreate: Mock = config => {
     }
   }]
 }
-
 export const mockSession: Mock = (config) => {
   return [200, {
     jwt: faker.random.word()
