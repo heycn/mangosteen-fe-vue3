56155c7bfd0ff278b0474366aa610eab45271cf3
heycn <heycn@foxmail.com>
2023-04-06 23:01:52 +0800

    修改文本

--- src/layouts/TimeTabsLayout.tsx
+++ src/layouts/TimeTabsLayout.tsx
@@ -73,7 +73,7 @@ export const TimeTabsLayout = defineComponent({
     return () => (
       <MainLayout>
         {{
-          title: () => '山竹记账',
+          title: () => '在线记账',
           icon: () => <OverlayIcon />,
           default: () => (
             <>
--- src/views/SignInPage.tsx
+++ src/views/SignInPage.tsx
@@ -72,7 +72,7 @@ export const SignInPage = defineComponent({
             <div class={s.wrapper}>
               <div class={s.logo}>
                 <Icon class={s.icon} name="mangosteen" />
-                <h1 class={s.appName}>山竹记账</h1>
+                <h1 class={s.appName}>在线记账</h1>
               </div>
               <Form onSubmit={onSubmit}>
                 <FormItem label="邮箱地址" type="text"
